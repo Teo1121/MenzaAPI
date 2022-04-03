05d2399425669771dd1fedb244cdf58f03d32980
Teo1121 <teo.ferenac@gmail.com>
2022-04-03 19:34:33 +0200

    added discord bot

---
+++ src/menza_discord.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+import json
+
+class MyClient(discord.Client):
+    COMMANDS = {"!list"   : lambda args : requests.get("http://127.0.0.1:8081/menza").json(),
+                "!signup" : lambda args : requests.post("http://127.0.0.1:8081/email",json={"email":args[0]}).json(),
+                "!help"   : lambda args : {"message":"!list - check out today's menu!\n!signup <email> - signup to the email subscription"}}
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message : discord.message.Message):
+        if message.author == self.user:
+            return
+
+        print('Message from {0.author}: {0.content}'.format(message))
+        command = message.content.split(' ')
+
+        if command[0] in MyClient.COMMANDS:
+            response = MyClient.COMMANDS[command[0]](command[1:])
+            if 'data' in response:
+                await message.channel.send(json.dumps(response['data'], ensure_ascii=False, indent=4))
+            else:
+                await message.channel.send(response['message'])
+
+client = MyClient()
+client.run('OTU5ODQ5NTg2NTMxMzgxMzY4.Ykh3lg.rdubBNUVZDitMAuSTZr-AGUHaHo')
